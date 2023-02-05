89f061a9acb5b0b8dcc696eae0f5a7917113e94a
Armando Pineda <106399018+CarlosArmandoPineda@users.noreply.github.com>
2023-02-05 09:44:25 -0600

    Add files via upload
    5/2/2023

---
+++ curso c#/Metodos o Funciones/Funciones Estructura/Funciones Estructura/Funciones Estructura/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones_Estructura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Sintaxis
+            
+            Tipodevuelto nombreMetodo (parámetros){
+                cuerpo del método
+            }
+
+            -Cosas a tomar en cuenta:
+            1-No se ejecutará hasta su llamada.
+            2-Todos los métodos iran dentro de una clase.
+            3-Se debe especificar el tipo de vuelto y los parametros.
+            4-No hay distincion entre metodos y funciones en c#.
+
+            Ejemplo:
+            1)
+            Funcion Entera:
+
+            int sumaNumeros(){    //int : Tipo //sumaNumeros: Nombre
+                int num1=7;
+                int num2=9;                //cuerpo
+
+                int resultado=num1+num2;
+
+                return resultado; //valor devuelto
+            2)
+            Ejemplo con Parametros:
+
+            int sumaNumeros(int num1,int num2){    //int : Tipo //sumaNumeros: Nombre //() Parametros
+                             
+                int resultado=num1+num2;             //cuerpo
+
+                return resultado; //valor devuelto
+
+            }
+
+            3)
+            Funcion void: No retorna nada.
+
+            void sumaNumeros(){   
+                int num1=7;
+                int num2=9;                }
+
+                int resultado=num1+num2;
+
+                Console.WriteLine(resultado);
+            */
+        }
+    }
+}
