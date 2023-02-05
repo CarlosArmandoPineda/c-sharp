193ca06e56f7887dc5a740f684be86777c08061d
Armando Pineda <106399018+CarlosArmandoPineda@users.noreply.github.com>
2023-02-05 09:45:48 -0600

    Add files via upload
    5/2/2023

---
+++ curso c#/Metodos o Funciones/Funciones 2/Funciones 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sumaNumeros(7,8);
+        }
+
+        //Funcion Void con Parametros
+        static void sumaNumeros(int num1, int num2)
+        {
+            Console.WriteLine("El resultado es: " + (num1+num2));
+        }
+    }
+}
