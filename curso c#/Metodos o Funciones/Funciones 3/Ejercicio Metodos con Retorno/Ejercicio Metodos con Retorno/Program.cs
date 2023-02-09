2828ea8a90c16a03863d9d0db4de20cf02eaf13c
Armando Pineda <106399018+CarlosArmandoPineda@users.noreply.github.com>
2023-02-09 13:42:56 -0600

    Add files via upload
    9/2

---
+++ curso c#/Metodos o Funciones/Funciones 3/Ejercicio Metodos con Retorno/Ejercicio Metodos con Retorno/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Metodos_con_Retorno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sumaNumeros(7, 9));
+            Console.WriteLine(dividenumeros(18, 7));
+            Console.WriteLine(divide2(18, 7));
+        }
+
+        static int sumaNumeros(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        static double dividenumeros(double num1, double num2) => num1/num2;
+
+        static double divide2(double nu1,int nu2)
+        {
+            double resultado;
+
+            resultado = nu1 / nu2;
+            
+            return resultado;
+        }
+    }
+}
